77ac27baa44596d65f6b0274ad8824ee0e26242d
Patrick Miziewicz <36363132+patmizi@users.noreply.github.com>
2018-04-21 17:26:04 +1000

    created verify_user API endpoint.

--- code2learn-back-end/app.py
+++ code2learn-back-end/app.py
@@ -5,6 +5,22 @@ import uuid
 app = Chalice(app_name='code2learn-back-end')
 app.debug = True
 
+def verify_userid(id, client):
+    if len(id) <= 0 :
+        raise ValueError('No ID was provided')
+    try:
+        return client["users"].find_one({"_id": str(id)})
+    except Exception as e:
+        raise Exception("An error has occured when trying to verify the user: ", e.message)
+
+def get_login_token(username, password, client):
+    if len(username) <= 0 or len(password) <= 0 :
+        raise ValueError('Invalid credentials were provided')
+    try:
+        return client["users"].find_one({"username": username, "password": password}, {"_id": 1})
+    except Exception as e:
+        raise Exception("An error occured when trying to connect to the database: ", e.message)
+
 def generate_uuid():
     return uuid.uuid4()
 
@@ -35,34 +51,31 @@ def add_user():
     body = app.current_request.json_body
     db_client = connect__mongodb()
     if len(body["username"]) <= 0:
-        return Response(body='No username was provided', status_code=400, headers={'Content-Type': 'text/plain'})
+        return Response(body='No username was provided',
+                        status_code=400,
+                        headers={'Content-Type': 'text/plain'})
     matching_users = db_client["users"].find_one({ "username": body["username"] })
     if matching_users is not None:
-        return Response(body='Username is already in use', status_code=400, headers={'Content-Type': 'text/plain'})
+        return Response(body='Username is already in use',
+                        status_code=400,
+                        headers={'Content-Type': 'text/plain'})
     body["_id"] = str(generate_uuid())
     db_client["users"].insert_one(body)
     return { "res": body }
 
+@app.route('/verify-user', methods=['POST'])
+def verify_user():
+    body = app.current_request.json_body
+    db_client = connect__mongodb()
+    try:
+        token = get_login_token(body["username"], body["password"], db_client)
+        return { "token": token["_id"] }
+    except Exception as e:
+        return Response(
+            body='Could not verify the user: ' + e.message,
+            status_code=400,
+            headers={'Content-Type': 'text/plain'})
+
 @app.route('/get-events', methods=['POST'])
 def get_events():
     return { "hello" : "world" }
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
